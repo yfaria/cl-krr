1f261d577eeb9b3a6bf67c5d2ea3337b3413e0fc
yfaria <yuriluisfaria@gmail.com>
2020-02-13 13:09:56 -0300

    added the the file of the sketches of classes and parser

---
+++ classes.lisp
@@ -0,0 +1,88 @@
+;;; Assuming that all inputs are syntactically correct.
+
+;;maybe I should create a domain/range sent (declarationsent)
+
+;;consertar as classes
+
+(defconstant *logsymbols* '(AND OR NOT => <=>))
+(defconstant *quantsymbols* '(FORALL EXISTS))
+
+(defclass quantsent ()
+  ((variables :acessor variables)
+  (sentence :acessor sentence)))
+
+(defclass equation ()
+  ((term-1 :acessor term-1)
+   (term-2 :acessor term-2)))
+
+(defclass logsent ()
+  ((logoperator :acessor logoperator)
+   (sentences :acessor sentences)))
+;;No caso do or e do and, que são variáveis no número de argumentos; o que fazer?
+;;talvez binarizar seja adequado.
+
+(defclass relsent ()
+  ((relword :acessor relword)
+   (args :acessor args)))
+
+(defclass funterm ()
+  ((funword :acessor funword)
+   (args :acessor args)
+   (return-type :acessor return-type)))
+
+;;classe das constantes: (?) [type é o instance do que ela é, colocar subclass]
+(defclass word ()
+  (type :acessor type))
+
+;;nas variáveis, elas vão precisar assumir tipos específicos dependendo da
+;;sentença; além de que talvez seja relevante saber se é rowvar ou objvar.
+;;ver se essa coisa de saber se é row é util ou não
+(defclass variable ()
+  (name :acessor name)
+  (type :acessor type)
+  (row? :acessor type))
+
+;;fparse retorna a formula f-in parseada.
+;;q é uma fila (ainda não adaptei o código pros casos de indeterminação)
+;;c é o contexto da transformação.
+;;Ele diz se o termo é função, relação ou uma constante.
+
+;; A VER: c pode ser uma hash table; se a palavra for levada num par, é função
+;;(no caso, o par seria uma lista com o tipo dos argumentos, domain;
+;;e o segundo elemento é o tipo do retorno, range), se num par com
+;;segundo elemento vazio, é relação (relação não tem range); se for levada num
+;;tipo é uma constante (ou tratar como função de argumento 0, a ver).
+
+;;Além disso, p/ descobrir que uma palavra é uma relword, não basta ver se é
+;;instância de relation; é preciso armazenar todas
+
+(defun fparse (c f-in f-out q)
+  (let ((head (car f-in))) ;; VER: se f-in não é uma lista (constante ou var).
+    (cond
+      ((member head *quantsymbols*) 
+       (let (sent (make-instance 'quantsent))
+	 (setf (variables sent) (cadr f-in)) ;;parsear as variáveis (*)
+	 (setf (sentence sent) (fparse c (cddr f-in) f-out q)) ;;?
+	 sent))
+      ((equal head '=)
+       (let (sent (make-instance 'equation))
+	 (setf (term-1 sent) (fparse c (cadr f-in) f-out q))
+	 (setf (term-2 sent) (fparse c (caddr f-in) f-out q))
+	 sent))
+      ((member head *logsymbols*)
+       (let (sent (make-instance 'logsent))
+	 (mapcar #'(lambda (sent-in)
+		     (funcall #'fparse c sent-in f-out q))
+		 (cdr sent)))) ;;conferir se isso está certo
+      ;;daqui, ou é um relsent, ou um funterm ou uma constante, ou variável.
+      ((member (string head) '(#\? #\@)) ;jogar lista p fora como constante (?)
+       (let (sent (make-instance 'variable))
+	 ;;tipo(?)
+	 (setf (name sent) ())
+	 ))
+      )))
+
+;;;(*) Esse caso precisa ser visto a parte, visto que vai ser uma lista de
+;;;variáveis, o que não é previsto pro fparse fazer. E.g.:
+;;;(forall (?X ?Y ?Z) (...)); precisa parsear (?X ?Y ?Z) como 3 variáveis;
+;;;ele vai tentar ler como relação ou função (ou não; talvez ele não avalie yz)
